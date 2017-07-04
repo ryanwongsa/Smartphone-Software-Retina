263c79af5882a62596c60853ad97826382069347
ryanwongsa <ryanwong1993@gmail.com>
2017-07-04 21:05:14 +0100

    Added base configuration for iOS with OpenCV

---
+++ RetinaVision/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RetinaVision
+//
+//  Created by Ryan Wong on 2017/07/04.
+//  Copyright © 2017 Ryan Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RetinaVision/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RetinaVision
+//
+//  Created by Ryan Wong on 2017/07/04.
+//  Copyright © 2017 Ryan Wong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
